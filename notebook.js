40a18745668bd107ba2ab75eda35412cba2aaec7
Hanny Maharani <maharaniulfa90@gmail.com>
2021-09-12 04:49:58 +0700

    Update notebook.js

--- notebook.js
+++ notebook.js
@@ -364,19 +364,3 @@
     }
 
 }).call(this);
-
-    © 2021 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
-Loading complete
